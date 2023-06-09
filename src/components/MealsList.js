732eeff674cbef9fe71cd4f8355939d8540e9448
Shivani Rathor <120269305+Shivanirathor@users.noreply.github.com>
2023-06-09 22:25:33 +0530

    Update MealsList.js

--- src/components/MealsList.js
+++ src/components/MealsList.js
@@ -1,20 +1,25 @@
-import React from "react";
+// src/components/MealsList/MealsList.js
+import React, { useContext } from 'react';
+import { MealsContext } from '../MealsProvider/MealsProvider';
 
 const MealsList = () => {
+  const { todaysMeals, tickMeal } = useContext(MealsContext);
 
-    return (
-        <div>
-            <h2>Meals:</h2>
-            <ul>
-                <li>
-                    <input 
-                        type="checkbox"
-                    />
-                    mealName
-                </li>
-            </ul>
+  return (
+    <div>
+      <h2>Meals List</h2>
+      {todaysMeals.map((meal) => (
+        <div key={meal.id}>
+          <input
+            type="checkbox"
+            checked={meal.ticked}
+            onChange={() => tickMeal(meal.id)}
+          />
+          <span>{meal.name}</span>
         </div>
-    )
-}
+      ))}
+    </div>
+  );
+};
 
 export default MealsList;
