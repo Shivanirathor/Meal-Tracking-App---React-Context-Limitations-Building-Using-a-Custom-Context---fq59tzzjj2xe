ac3f08619b1878a85b96fb8533333413a442e9f7
Shivani Rathor <120269305+Shivanirathor@users.noreply.github.com>
2023-06-10 16:08:45 +0530

    Update MealsProvider.js

--- src/components/MealsProvider.js
+++ src/components/MealsProvider.js
@@ -1,28 +1,88 @@
-// src/components/MealsProvider/MealsProvider.js
-import React, { createContext, useState } from 'react';
+import React, { useState } from "react";
+import { createContext } from "react";
 
-export const MealsContext = createContext();
+const todaysMeals = [
+    { id: 1, name: 'Baked Beans', checked: false },
+    { id: 2, name: 'Baked Sweet Potatoes', checked: false },
+    { id: 3, name: 'Baked Potatoes', checked: false }
+];
+
+const MealsContext = createContext();
 
 const MealsProvider = ({ children }) => {
-  const [todaysMeals, setTodaysMeals] = useState([
-    { id: 1, name: 'Breakfast', ticked: false },
-    { id: 2, name: 'Lunch', ticked: false },
-    { id: 3, name: 'Dinner', ticked: false },
-  ]);
 
-  const tickMeal = (mealId) => {
-    setTodaysMeals((prevMeals) =>
-      prevMeals.map((meal) =>
-        meal.id === mealId ? { ...meal, ticked: !meal.ticked } : meal
-      )
+    const [meals, setMeals] = useState(todaysMeals);
+
+    const tickMeal = (event) => {
+        const value = event.target.value;
+
+        const checkedMeal = meals.map((meal) => {
+            if (meal.id === Number(value)) {
+                return {
+                    ...meal,
+                    checked: !meal.checked
+                }
+            }
+
+            return meal;
+        });
+
+        setMeals(checkedMeal);
+    };
+
+    return (
+        <div>
+            <MealsContext.Provider value={{ meals, tickMeal }}>
+                {children}
+            </MealsContext.Provider>
+        </div>
     );
-  };
-
-  return (
-    <MealsContext.Provider value={{ todaysMeals, tickMeal }}>
-      {children}
-    </MealsContext.Provider>
-  );
 };
 
+export { MealsContext };
 export default MealsProvider;
+
+//Counter.js
+
+import React from "react";
+import { useContext } from "react";
+import { MealsContext } from "./MealsProvider";
+
+const Counter = () => {
+
+    const { meals } = useContext(MealsContext);
+
+    const remainingMealsCount = meals.filter((meal) => meal.checked === false).length;
+
+    return (
+        <div>
+            <h3>Meals Remaining: {remainingMealsCount}</h3>
+        </div>
+    )
+}
+
+export default Counter;
+
+//MealList.js
+
+import React from "react";
+import { useContext } from "react";
+import { MealsContext } from "./MealsProvider";
+
+const MealsList = () => {
+
+    const { meals, tickMeal } = useContext(MealsContext);
+
+    return (
+        <div>
+            <h2>Meals:</h2>
+            <ul>
+                {meals.map((meal) => {
+                    return <li key={meal.id}><input type="checkbox" value={meal.id} checked={meal.checked} onChange={tickMeal} />{meal.name}</li>
+                })}
+            </ul>
+        </div>
+    )
+}
+
+export default MealsList;
