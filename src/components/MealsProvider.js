4dafe38626a08bbe9e0d40b3e85d5c00bd3c4c3b
Shivani Rathor <120269305+Shivanirathor@users.noreply.github.com>
2023-06-10 16:10:51 +0530

    Update MealsProvider.js

--- src/components/MealsProvider.js
+++ src/components/MealsProvider.js
@@ -42,47 +42,4 @@ const MealsProvider = ({ children }) => {
 export { MealsContext };
 export default MealsProvider;
 
-//Counter.js
 
-import React from "react";
-import { useContext } from "react";
-import { MealsContext } from "./MealsProvider";
-
-const Counter = () => {
-
-    const { meals } = useContext(MealsContext);
-
-    const remainingMealsCount = meals.filter((meal) => meal.checked === false).length;
-
-    return (
-        <div>
-            <h3>Meals Remaining: {remainingMealsCount}</h3>
-        </div>
-    )
-}
-
-export default Counter;
-
-//MealList.js
-
-import React from "react";
-import { useContext } from "react";
-import { MealsContext } from "./MealsProvider";
-
-const MealsList = () => {
-
-    const { meals, tickMeal } = useContext(MealsContext);
-
-    return (
-        <div>
-            <h2>Meals:</h2>
-            <ul>
-                {meals.map((meal) => {
-                    return <li key={meal.id}><input type="checkbox" value={meal.id} checked={meal.checked} onChange={tickMeal} />{meal.name}</li>
-                })}
-            </ul>
-        </div>
-    )
-}
-
-export default MealsList;
